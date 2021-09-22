2ba9e404a2b0adc923b45564d4e0c7a5d08e1c1a
Prathmesh Godse <pgodse97@gmail.com>
2021-09-22 14:37:47 +0530

    removed attempt 1 : moblie drag and drop code

--- script.js
+++ script.js
@@ -178,36 +178,6 @@ function setDrop(e) {
   }
 }
 
-function touchHandler(event) {
-  var touch = event.changedTouches[0];
-
-  var simulatedEvent = document.createEvent("MouseEvent");
-  simulatedEvent.initMouseEvent(
-    {
-      touchstart: "mousedown",
-      touchmove: "mousemove",
-      touchend: "mouseup",
-    }[event.type],
-    true,
-    true,
-    window,
-    1,
-    touch.screenX,
-    touch.screenY,
-    touch.clientX,
-    touch.clientY,
-    false,
-    false,
-    false,
-    false,
-    0,
-    null
-  );
-
-  touch.target.dispatchEvent(simulatedEvent);
-  event.preventDefault();
-}
-
 //
 //
 //Event Listners
@@ -239,8 +209,3 @@ taskListContainer.addEventListener("dragstart", (e) => setDragging(e.target));
 taskListContainer.addEventListener("dragend", (e) => setDragging(e.target));
 
 taskListContainer.addEventListener("dragover", (e) => setDrop(e));
-
-document.addEventListener("touchstart", touchHandler, true);
-document.addEventListener("touchmove", touchHandler, true);
-document.addEventListener("touchend", touchHandler, true);
-document.addEventListener("touchcancel", touchHandler, true);
